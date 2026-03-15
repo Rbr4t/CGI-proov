f3b5ff8d78dd4b23bfc080b7e7453c20e0c71fa6
Robert Koor <robertkoor0@gmail.com>
2026-03-16 00:24:04 +0200

    fixed a missed annotation

--- src/main/java/com/example/restaurant/repo/TableRepository.java
+++ src/main/java/com/example/restaurant/repo/TableRepository.java
@@ -3,9 +3,11 @@ package com.example.restaurant.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.restaurant.model.Table;
 
+@Repository
 public interface TableRepository extends JpaRepository<Table, Long> {
     List<Table> findfindByZone(String zone);
 
