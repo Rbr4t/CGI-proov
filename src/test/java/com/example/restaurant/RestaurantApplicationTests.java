bc584dd2b5ca80ac721bc36d720381d156c21aa7
Robert Koor <robertkoor0@gmail.com>
2026-03-15 23:45:20 +0200

    project structure

--- src/test/java/com/example/restaurant/RestaurantApplicationTests.java
+++ src/test/java/com/example/restaurant/RestaurantApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.example.restaurant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class RestaurantApplicationTests {
 
 	@Test
 	void contextLoads() {
